06320d4c27937ac64deb33d69bd26c2d00323542
KristelJeanneB <krid.bautista.up@phinmaed.com>
2024-03-16 17:02:35 +0800

    favicon and secondary logo

--- app/src/main/java/com/example/christianjamesrapelo/block1/christianjamesrapelo/block1/trial2/fragments/CheckoutFragment.kt
+++ app/src/main/java/com/example/christianjamesrapelo/block1/christianjamesrapelo/block1/trial2/fragments/CheckoutFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.christianjamesrapelo.block1.christianjamesrapelo.block1.trial2.R
-
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
